cc9a3ddb076e290b2bab42deb076aff9e24718b5
Akhilps118 <87316613+Akhilps118@users.noreply.github.com>
2022-02-18 23:28:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,34 @@
-provider "aws" {
-  region = "us-east-2"
+resource "aws_s3_bucket" "static_site"  {
+  bucket = "${var.bucket_name}"
+}
+resource "aws_s3_bucket_acl" "static_site"  {
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
+
+}
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = "${var.bucket_name}"
+
+  index_document {
+    suffix = "index.html"
+  }
+    error_document {
+    key = "error.html"
+  }
 }
 
-module "s3-website" {
-  source = "../modules/s3-website"
-
-  bucket_name = "skillsit-s3-website"
+resource "aws_s3_object" "index" {
+  bucket       = "${var.bucket_name}"
+  key          = "index.html"
+  source       = "src/index.html"
+  content_type = "text/html"
+  acl          = "public-read"
 }
 
-output "website_endpoint" {
-  value = module.s3-website.website_endpoint
+resource "aws_s3_object" "error" {
+  bucket       = "${var.bucket_name}"
+  key          = "error.html"
+  source       = "src/error.html"
+  content_type = "text/html"
+  acl          = "public-read"
 }
